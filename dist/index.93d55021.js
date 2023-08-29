8e2a07489707da77dab170ac16dfc83abe61f4c2
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2023-08-29 12:26:08 +0545

    recursiverly render the children

--- dist/index.93d55021.js
+++ dist/index.93d55021.js
@@ -1,9 +1,16 @@
+const Zitact = {
+    createElement,
+    render
+};
 function createElement(type, props, ...children) {
     return {
         type,
         props: {
             ...props,
-            children: children.map((child)=>typeof child === "object" ? child : createTextElement(child))
+            /* here we are checking this because the children of any react element can just be text
+       or number //////// (primitives).  So, we have to handle them seperately as while rendernig
+       recursively it can be a problem.
+      */ children: children.map((child)=>typeof child === "object" ? child : createTextElement(child))
         }
     };
 }
@@ -16,52 +23,78 @@ function createTextElement(text) {
         }
     };
 }
+const el = {
+    type: "div",
+    props: {
+        className: "id",
+        children: [
+            {
+                type: "h1",
+                props: {
+                    children: [
+                        {
+                            type: "TEXT_ELEMENT",
+                            props: {
+                                nodeValue: "Hello World",
+                                children: []
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                type: "p",
+                props: {
+                    children: [
+                        {
+                            type: "TEXT_ELEMENT",
+                            props: {
+                                nodeValue: "lorem",
+                                children: []
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+};
 function render(element, container) {
-    // TODO: create dom nodes
-    const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
-    const isProperty = (key)=>key !== "children";
-    Object.keys(element.props).filter(isProperty).forEach((name)=>{
-        dom[name] = element.props[name];
+    const dom = element.type == "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
+    //filering children from the props and assigning remaining to the dom
+    Object.keys(element.props).filter((key)=>key !== "children").forEach((name)=>dom[name] = element.props[name]);
+    /*
+    Recursiverly going through the childrren and appending to the parent container after finish of each 
+    children pass.
+    */ element.props.children.forEach((child)=>{
+        render(child, dom);
     });
-    element.props.children.forEach((child)=>render(child, dom));
-    console.log(dom);
     container.appendChild(dom);
 }
-const Zitact = {
-    createElement,
-    render
-};
 /** @jsx Zitact.createElement */ const element = /*#__PURE__*/ Zitact.createElement("div", {
     className: "id",
     __source: {
         fileName: "index.jsx",
-        lineNumber: 51,
+        lineNumber: 95,
         columnNumber: 3
     },
     __self: this
 }, /*#__PURE__*/ Zitact.createElement("h1", {
     __source: {
         fileName: "index.jsx",
-        lineNumber: 52,
+        lineNumber: 96,
         columnNumber: 5
     },
     __self: this
 }, "Hello world"), /*#__PURE__*/ Zitact.createElement("p", {
     __source: {
         fileName: "index.jsx",
-        lineNumber: 53,
+        lineNumber: 97,
         columnNumber: 5
     },
     __self: this
-}, "lorem"));
+}, "Lorem eiciendis eos laborum deserunt vero adipisci aliquid voluptates ratione ex!"));
 const root = document.getElementById("root");
-Zitact.render(element, root); // console.log(element);
- // React.createElement('h1', {class: 'heading'}, React.createElement())
- // const element = Zitact.createElement(
- //   "div",
- //   { id: "app" },
- //   Zitact.createElement("a", null, "bar"),
- //   Zitact.createElement("b")
- // );
+Zitact.render(element, root);
 
 //# sourceMappingURL=index.93d55021.js.map
--- index.jsx
+++ index.jsx
@@ -1,8 +1,17 @@
+const Zitact = {
+  createElement,
+  render,
+};
+
 function createElement(type, props, ...children) {
   return {
     type,
     props: {
       ...props,
+      /* here we are checking this because the children of any react element can just be text
+       or number //////// (primitives).  So, we have to handle them seperately as while rendernig
+       recursively it can be a problem.
+      */
       children: children.map((child) =>
         typeof child === "object" ? child : createTextElement(child)
       ),
@@ -19,50 +28,76 @@ function createTextElement(text) {
     },
   };
 }
-function render(element, container) {
-  // TODO: create dom nodes
-  const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
 
-  const isProperty = (key) => key !== "children";
 
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name];
-    });
-
-  element.props.children.forEach((child) => render(child, dom));
-
-  console.log(dom);
-
-  container.appendChild(dom);
+const el = {
+  type: 'div', 
+  props: {
+    className: 'id',
+    children:[
+      {
+        type: 'h1',
+        props: {
+          children: [
+            {
+              type: 'TEXT_ELEMENT',
+              props: {
+                nodeValue: 'Hello World',
+                children:[],
+             }
+            }
+          ]
+        }
+      },
+      {
+        type: 'p',
+        props:{
+          children:[ 
+          {
+            type: "TEXT_ELEMENT",
+            props: {
+              nodeValue: 'lorem',
+              children: []
+            }
+          }
+          ]
+        }
+      }
+    ]
+  }
 }
 
-const Zitact = {
-  createElement,
-  render,
-};
+function render(element, container) {
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+ 
+  //filering children from the props and assigning remaining to the dom
+  Object.keys(element.props)
+    .filter(key => key!=='children')
+    .forEach((name) => (dom[name] = element.props[name]));
+
+    /*
+    Recursiverly going through the childrren and appending to the parent container after finish of each 
+    children pass.
+    */
+    element.props.children.forEach(child=> {
+      render(child, dom)
+    })
+
+    container.appendChild(dom);
+    
+}
 
 /** @jsx Zitact.createElement */
 const element = (
   <div className="id">
     <h1>Hello world</h1>
-    <p>lorem</p>
+    <p>Lorem eiciendis eos laborum deserunt vero adipisci aliquid voluptates ratione ex!</p>
   </div>
 );
 
 const root = document.getElementById("root");
 
 Zitact.render(element, root);
-
-// console.log(element);
-// React.createElement('h1', {class: 'heading'}, React.createElement())
-// const element = Zitact.createElement(
-//   "div",
-//   { id: "app" },
-//   Zitact.createElement("a", null, "bar"),
-//   Zitact.createElement("b")
-// );
